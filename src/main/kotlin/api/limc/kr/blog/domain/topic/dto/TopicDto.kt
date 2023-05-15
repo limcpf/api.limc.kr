73cc0bd6c01a807623cdaf404cb2f5a5d45d1d6a
limcpf <matathresh1740@naver.com>
2023-05-16 00:02:45 +0900

    feat: Topic 관련 Test 생성

--- src/main/kotlin/api/limc/kr/blog/domain/topic/dto/TopicDto.kt
+++ src/main/kotlin/api/limc/kr/blog/domain/topic/dto/TopicDto.kt
@@ -2,4 +2,6 @@ package api.limc.kr.blog.domain.topic.dto
 
 import api.limc.kr.blog.domain.site.dto.SiteDto
 
-data class TopicDto(val id: Long?, val site:SiteDto, val name: String?)
+data class TopicDto(val id: Long?, val site:SiteDto, val name: String?) {
+    constructor(site: SiteDto, name: String?):this(null, site, name)
+}
--- src/test/kotlin/api/limc/kr/blog/domain/topic/TopicControllerTest.kt
+++ src/test/kotlin/api/limc/kr/blog/domain/topic/TopicControllerTest.kt
@@ -1,4 +1,45 @@
 package api.limc.kr.blog.domain.topic
 
+import api.limc.kr.blog.domain.site.SiteController
+import api.limc.kr.blog.domain.site.dto.SiteDto
+import api.limc.kr.blog.domain.topic.dto.TopicDto
+import api.limc.kr.blog.shared.LimcTest
+import api.limc.kr.blog.shared.enums.Pages
+import org.junit.jupiter.api.Assertions
+import org.junit.jupiter.api.BeforeAll
+import org.junit.jupiter.api.Order
+import org.junit.jupiter.api.Test
+import org.springframework.beans.factory.annotation.Autowired
+import org.springframework.data.domain.PageRequest
+
+@LimcTest
 class TopicControllerTest {
+    @Autowired lateinit var controller: TopicController
+    @Autowired lateinit var siteController: SiteController
+
+    lateinit var site: SiteDto
+    lateinit var topic: TopicDto
+    @BeforeAll
+    fun setup() {
+        val siteDto = SiteDto("dev")
+        site = siteController.save(siteDto)
+    }
+
+    @Test
+    @Order(0)
+    fun saveTest() {
+        val topicDto = TopicDto(site, "test Topic")
+        topic = controller.save(topicDto)
+
+        Assertions.assertNotNull(topic)
+    }
+
+    @Test
+    @Order(1)
+    fun findAllBySite() {
+        val site = "DEV"
+        val topics = controller.findAll(site, PageRequest.of(0, Pages.TOPIC_PAGE_NUM.value))
+
+        Assertions.assertTrue(!topics.isEmpty)
+    }
 }
