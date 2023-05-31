4323bf010416ec95d219d998c425fc763380c935
limcpf <matathresh1740@naver.com>
2023-05-31 22:05:23 +0900

    feat: test 코드 정리

--- src/test/kotlin/api/limc/kr/blog/domain/site/SitePrivateControllerTest.kt
+++ src/test/kotlin/api/limc/kr/blog/domain/site/SitePrivateControllerTest.kt
@@ -1,24 +1,29 @@
 package api.limc.kr.blog.domain.site
 
+import api.limc.kr.blog.domain.admin.AdminController
+import api.limc.kr.blog.domain.admin.dto.AdminDto
 import api.limc.kr.blog.domain.site.dto.SiteDto
 import api.limc.kr.blog.shared.LimcTest
 import com.fasterxml.jackson.databind.ObjectMapper
 import com.fasterxml.jackson.databind.SerializationFeature
 import com.fasterxml.jackson.datatype.jsr310.JavaTimeModule
 import org.junit.jupiter.api.Assertions
+import org.junit.jupiter.api.BeforeAll
 import org.junit.jupiter.api.Order
 import org.junit.jupiter.api.Test
 import org.springframework.beans.factory.annotation.Autowired
 import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc
 import org.springframework.http.MediaType
 import org.springframework.http.converter.json.Jackson2ObjectMapperBuilder
-import org.springframework.mock.web.MockHttpServletResponse
 import org.springframework.test.web.servlet.MockMvc
+import org.springframework.test.web.servlet.MvcResult
 import org.springframework.test.web.servlet.request.MockMvcRequestBuilders
 
 @AutoConfigureMockMvc
 @LimcTest
 class SitePrivateControllerTest {
+    /** env setting **/
+    @Autowired lateinit var adminController: AdminController
     @Autowired lateinit var controller: SitePrivateController
     @Autowired lateinit var mockMvc: MockMvc
     val objectMapper: ObjectMapper = Jackson2ObjectMapperBuilder.json()
@@ -27,6 +32,15 @@ class SitePrivateControllerTest {
             .build()
 
     val name = "DEV"
+    var accessToken: String? = null
+
+    @BeforeAll
+    fun login() {
+        val adminDto = AdminDto(null, "id", "pw")
+        adminController.save(adminDto)
+        val response = adminController.login(adminDto)
+        accessToken = "Bearer " + response.body?.accessToken
+    }
 
     @Test
     fun testsave() {
@@ -34,18 +48,20 @@ class SitePrivateControllerTest {
 
         val siteDto = SiteDto(name)
         val jsonAsString: String = objectMapper.writeValueAsString(siteDto)
-        println(jsonAsString)
-        mockMvc
-            .perform(MockMvcRequestBuilders.post(url).content(jsonAsString).contentType(MediaType.APPLICATION_JSON))
-            .andExpect { result ->
-                run {
-                    val res: MockHttpServletResponse = result.response
-                    println(res.errorMessage)
-                    println(res.contentAsString)
-                }
-            }
 
-        println(controller.findAll().size)
+        val request = MockMvcRequestBuilders.post(url)
+            .content(jsonAsString)
+            .contentType(MediaType.APPLICATION_JSON)
+            .header("Authorization", accessToken)
+
+        val callback = {r: MvcResult -> run {
+            Assertions.assertTrue(r.response.errorMessage.isNullOrBlank())
+            Assertions.assertTrue(r.response.contentAsString.isNotBlank())
+        }}
+
+        mockMvc
+            .perform(request)
+            .andExpect(callback)
     }
 
     @Test
